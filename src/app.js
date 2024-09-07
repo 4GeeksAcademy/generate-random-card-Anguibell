1a11fabd0bf5cec27ba269187f391fb9caaacfec
Anguibell <romeroanguibell17@gmail.com>
2024-09-07 12:11:43 +0000

    Finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pintas = ["♦", "♥", "♠", "♣"];
+let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+function randomPinta() {
+  let randomPinta = Math.floor(Math.random() * pintas.length);
+  return pintas[randomPinta];
+}
+
+function randomValue() {
+  let randomValue = Math.floor(Math.random() * values.length);
+  return values[randomValue];
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pinta = randomPinta();
+  let pintaTop = document.getElementById("pinta-top");
+  pintaTop.innerHTML = pinta;
+  let pintaBottom = document.getElementById("pinta-bottom");
+  pintaBottom.innerHTML = pinta;
+  document.getElementById("value").innerHTML = randomValue();
+
+  if (pinta == "♦" || pinta == "♥") {
+    pintaTop.style.color = "red";
+    pintaBottom.style.color = "red";
+  }
 };
